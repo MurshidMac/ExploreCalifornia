5a371c13bfd820484624804970f470b4d8ae047c
murshidmac <mh.murshidco.uk@outlook.com>
2018-07-15 19:03:48 +0530

    Template Data Tested

--- src/main/java/com/example/ec/explocali/ExplocaliApplication.java
+++ src/main/java/com/example/ec/explocali/ExplocaliApplication.java
@@ -1,12 +1,35 @@
 package com.example.ec.explocali;
 
+import com.example.ec.explocali.services.TourPackageService;
+import com.example.ec.explocali.services.TourService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExplocaliApplication {
+public class ExplocaliApplication implements CommandLineRunner {
+	@Autowired
+	private TourService tourService;
+	@Autowired
+	private TourPackageService tourPackageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExplocaliApplication.class, args);
+	}
+
+	// This is to check for the data Dynamically wether its working properly
+	@Override
+	public void run(String... args) throws Exception {
+		tourPackageService.createTourPackage("BC", "Backpack Cal");
+		tourPackageService.createTourPackage("CC", "California Calm");
+		tourPackageService.createTourPackage("CH", "California Hot springs");
+		tourPackageService.createTourPackage("CY", "Cycle California");
+		tourPackageService.createTourPackage("DS", "From Desert to Sea");
+		tourPackageService.createTourPackage("KC", "Kids California");
+		tourPackageService.createTourPackage("NW", "Nature Watch");
+		tourPackageService.createTourPackage("SC", "Snowboard Cali");
+		tourPackageService.createTourPackage("TC", "Taste of California");
+		tourPackageService.lookup().forEach(tourPackage -> System.out.println(tourPackage));	//
 	}
 }
