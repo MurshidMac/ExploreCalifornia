4109c7ec9c507866d92f53a07e8b7ee221fc2f84
murshidmac <mh.murshidco.uk@outlook.com>
2018-07-15 18:11:43 +0530

    Created The Spring Data JPA which allows us to do more CRUD Operation

---
+++ src/main/java/com/example/ec/explocali/Repo/TourPackageRepository.java
@@ -0,0 +1,7 @@
+package com.example.ec.explocali.Repo;
+
+import com.example.ec.explocali.domain.TourPackage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+}
---
+++ src/main/java/com/example/ec/explocali/Repo/TourRepository.java
@@ -0,0 +1,8 @@
+package com.example.ec.explocali.Repo;
+
+import com.example.ec.explocali.domain.Tour;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+}
