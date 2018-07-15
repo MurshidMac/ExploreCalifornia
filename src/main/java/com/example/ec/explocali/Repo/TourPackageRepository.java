0aeec791f8ccfe154dc033e77d70d56ac6b2e59c
murshidmac <mh.murshidco.uk@outlook.com>
2018-07-15 21:15:07 +0530

    Searching with Strings

--- src/main/java/com/example/ec/explocali/Repo/TourPackageRepository.java
+++ src/main/java/com/example/ec/explocali/Repo/TourPackageRepository.java
@@ -2,7 +2,8 @@ package com.example.ec.explocali.Repo;
 
 import com.example.ec.explocali.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
-    TourPackage findByName(String name);
+    TourPackage findByName(@Param("name") String name);
 }
--- src/main/java/com/example/ec/explocali/Repo/TourRepository.java
+++ src/main/java/com/example/ec/explocali/Repo/TourRepository.java
@@ -2,7 +2,10 @@ package com.example.ec.explocali.Repo;
 
 import com.example.ec.explocali.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
-
+    List<Tour> findByTourPackageCode(@Param("name") String code);
 }
