84d49e2688136c42a6eb5e1d25d5d2794e34162e
murshidmac <mh.murshidco.uk@outlook.com>
2018-07-15 21:33:08 +0530

    Controlling added to Tour and TourPackages

--- src/main/java/com/example/ec/explocali/Repo/TourRepository.java
+++ src/main/java/com/example/ec/explocali/Repo/TourRepository.java
@@ -1,11 +1,38 @@
 package com.example.ec.explocali.Repo;
 
 import com.example.ec.explocali.domain.Tour;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
+// Controlling the api for external changes
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(@Param("name") String code);
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    Page<Tour> findByTourPackageCode(@Param("name") String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
