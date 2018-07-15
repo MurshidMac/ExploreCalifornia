b560f27a107e8cd355c877a495313dc5a2635638
murshidmac <mh.murshidco.uk@outlook.com>
2018-07-15 16:14:26 +0530

    Explorer Microservice

---
+++ src/main/java/com/example/ec/explocali/ExplocaliApplication.java
@@ -0,0 +1,12 @@
+package com.example.ec.explocali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplocaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplocaliApplication.class, args);
+	}
+}
