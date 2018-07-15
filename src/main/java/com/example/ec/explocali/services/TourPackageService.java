ffc9241e6d4b61ef01b526eed08f21cb1988274b
murshidmac <mh.murshidco.uk@outlook.com>
2018-07-15 18:53:11 +0530

    Included all the services

---
+++ src/main/java/com/example/ec/explocali/services/TourPackageService.java
@@ -0,0 +1,32 @@
+package com.example.ec.explocali.services;
+
+import com.example.ec.explocali.Repo.TourPackageRepository;
+import com.example.ec.explocali.domain.TourPackage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TourPackageService {
+    private TourPackageRepository  tourPackageRepository;
+
+    @Autowired
+    public TourPackageService(TourPackageRepository tourPackageRepository) {
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    public TourPackage createTourPackage(String code, String name){
+        // if it doesn't exist create a tour package
+        if(!tourPackageRepository.existsById(code)){
+            tourPackageRepository.save(new TourPackage(code, name));
+        }
+        return null;
+    }
+
+    public Iterable<TourPackage> lookup(){
+        return tourPackageRepository.findAll();
+    }
+
+    public long total(){
+        return tourPackageRepository.count();
+    }
+}
---
+++ src/main/java/com/example/ec/explocali/services/TourService.java
@@ -0,0 +1,55 @@
+package com.example.ec.explocali.services;
+
+import com.example.ec.explocali.Repo.TourPackageRepository;
+import com.example.ec.explocali.Repo.TourRepository;
+import com.example.ec.explocali.domain.Difficulty;
+import com.example.ec.explocali.domain.Region;
+import com.example.ec.explocali.domain.Tour;
+import com.example.ec.explocali.domain.TourPackage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TourService {
+
+
+    private TourRepository tourRepository;
+    private TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourService(TourRepository tourRepository, TourPackageRepository tourPackageRepository) {
+        this.tourRepository = tourRepository;
+        this.tourPackageRepository= tourPackageRepository;
+    }
+
+    public Tour createTour(String title, String description, String blurb,
+                           Integer price, String duration, String bullets, String keywords,
+                           String TourpackCode, Difficulty diff, Region region){
+        // before we create a tour package we should know wether the tour package exists in
+        // the db
+        Optional<TourPackage> searchtourpacke = tourPackageRepository.findById(TourpackCode);
+        // This is to check for the changes happened in the 1.8 java
+        // With few Name changes
+        System.out.println(searchtourpacke.toString());
+
+        if(!searchtourpacke.isPresent()){
+            throw new RuntimeException("Tour package does not exists");
+        }
+
+        // This is getting from the list optional class
+        TourPackage packageFromOptional = searchtourpacke.get();
+        return tourRepository.save(new Tour(title, description,blurb, price,duration,bullets,
+                keywords,packageFromOptional,diff,region));
+    }
+
+    public Iterable<Tour> loopup(){
+        return tourRepository.findAll();
+    }
+
+    public long total(){
+        return tourRepository.count();
+    }
+
+}
