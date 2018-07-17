f2cfacdd5210dc81138f76509ef9bc905bc71b5c
murshidmac <mh.murshidco.uk@outlook.com>
2018-07-17 06:58:23 +0530

    JUNIT Test implemented

---
+++ src/test/java/com/example/ec/explocali/TourPackageTest.java
@@ -0,0 +1,12 @@
+package com.example.ec.explocali;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+// Test for TourPackageService
+// Writing test cases followed with this https://www.youtube.com/watch?v=8S8o46avgAw
+// for service test using this class https://www.youtube.com/watch?v=Ijj5RLSAcYw
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TourPackageTest {
+
+}
